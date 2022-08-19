7f3feeaf37f46bec4860127ec1394ee47f548565
Nguyễn Ngọc Phước <phuoc.nguyen@heatmob.net>
2022-08-19 15:35:47 +0700

    cancel previous

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! CI/CD deployed, run but cancel previous build for sure')
+    res.send('Hello World! CI/CD deployed')
 })
 
 app.listen(port, () => {
