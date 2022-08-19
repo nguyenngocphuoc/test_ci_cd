3a94cf8275f5bb22c835618bad1d54fcd7100b06
Nguyễn Ngọc Phước <phuoc.nguyen@heatmob.net>
2022-08-19 15:14:39 +0700

    deployed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! CI/CD deployed')
+    res.send('Hello World! CI/CD deployed, run but cancel previous build')
 })
 
 app.listen(port, () => {
