33ab447c2a7e3067ac9060786da66d1316f40dc4
Nguyễn Ngọc Phước <phuoc.nguyen@heatmob.net>
2022-08-19 15:26:38 +0700

    cancel previous

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}. now github runner work`)
+    console.log(`Example app listening on port ${port}.`)
 })
